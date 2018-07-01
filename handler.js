6af59f786958863f3cb7dd11a204117be3b65dd2
Fadee kannah <fadeekannah@gmail.com>
2018-07-01 14:33:06 -0700

    Tweak for personal use

--- handler.js
+++ handler.js
@@ -2,7 +2,6 @@
 
 // importing AWS sdk
 import AWS from 'aws-sdk';
-import request from 'request';
 // importing config file which contains AWS key
 // Best practice: to use a config.copy.json when pushing to github
 // Coz exposing the AWS keys to public is not good
@@ -18,103 +17,39 @@ AWS.config.update({
 let ses = new AWS.SES();
 
 // Structure of sendMail params structure:
-/*
-var params = {
-  Destination: {  / required /
-    BccAddresses: [
-      'STRING_VALUE',
-      / more items /
-    ],
-    CcAddresses: [
-      'STRING_VALUE',
-      / more items /
-    ],
-    ToAddresses: [
-      'STRING_VALUE',
-      / more items /
-    ]
-  },
-  Message: { / required /
-    Body: { / required /
-      Html: {
-        Data: 'STRING_VALUE', / required /
-        Charset: 'STRING_VALUE'
-      },
-      Text: {
-        Data: 'STRING_VALUE', / required /
-        Charset: 'STRING_VALUE'
-      }
-    },
-    Subject: { / required /
-      Data: 'STRING_VALUE', / required /
-      Charset: 'STRING_VALUE'
-    }
-  },
-  Source: 'STRING_VALUE', / required /
-  ConfigurationSetName: 'STRING_VALUE',
-  ReplyToAddresses: [
-    'STRING_VALUE',
-    / more items /
-  ],
-  ReturnPath: 'STRING_VALUE',
-  ReturnPathArn: 'STRING_VALUE',
-  SourceArn: 'STRING_VALUE',
-  Tags: [
-    {
-      Name: 'STRING_VALUE', / required /
-      Value: 'STRING_VALUE' / required /
-    },
-    / more items /
-  ]
-};
-
-ses.sendEmail(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-});
-
-*/
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/SES.html#sendEmail-property
 
 // The function to send SES email message
 module.exports.sendMail = (event, context, callback) => {
 
-  let bccEmailAddresses = event.body.bccEmailAddresses;
-  let ccEmailAddresses = event.body.ccEmailAddresses;
-  let toEmailAddresses = event.body.toEmailAddresses;
-  let bodyData = event.body.bodyData;
-  let bodyCharset = event.body.bodyCharset;
-  let subjectdata = event.body.subjectdata;
-  let subjectCharset = event.body.subjectCharset;
-  let sourceEmail = event.body.sourceEmail;
-  let replyToAddresses = event.body.replyToAddresses;
-
-// Building the slack message
-  var options = {
-    text: 'We have got a customer support from ' + replyToAddresses + ' Log into <https://privateemail.com/appsuite/> to answer their query.',
-  }
+  let toArray = event.body.to;
+  let bodyString = event.body.body;
+  let subjectString = event.body.subject;
+  let sourceString = event.body.from;
+  let replyToString = event.body.replyTo;
 
 // The parameters for sending mail using ses.sendEmail()
   let emailParams = {
     Destination: {
-      BccAddresses: bccEmailAddresses,
-      CcAddresses: ccEmailAddresses,
-      ToAddresses: toEmailAddresses
+      ToAddresses: toArray
     },
     Message: {
       Body: {
         Text: {
-          Data: bodyData,
-          Charset: bodyCharset
+          Data: bodyString,
+          Charset: 'UTF-8'
         }
       },
       Subject: {
-        Data: subjectdata,
-        Charset: subjectCharset
+        Data: subjectString,
+        Charset: 'UTF-8'
       }
     },
-    Source: sourceEmail,
-    ReplyToAddresses: replyToAddresses
+    Source: sourceString,
+    ReplyToAddresses: [replyToString]
   };
+
+  console.log(emailParams)
 
 // the response to send back after email success.
   const response = {
@@ -132,17 +67,7 @@ module.exports.sendMail = (event, context, callback) => {
       } else {
         console.log("SES successful");
         console.log(data);
-
-        request.post(config.slackWebhook, { body: JSON.stringify(options)}, function (err, httpResponse, body) {
-          if (err) {
-            console.error('Slack webhook failed:', err);
-            callback(err);
-          }
-          console.log('Post to slack bot successful!!');
-          console.log(httpResponse);
-          console.log('Post to slack bot replied with:', body);
-          callback(null, response);
-        });
+        callback(null, response);
       }
   });
 };
